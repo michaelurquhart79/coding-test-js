fe8fd655684f76549343574a1e0199697c39cb44
Mike Ritchie <mike@13coders.com>
2019-05-01 10:12:26 +0100

    Updated use of assert to avoid deprecated API

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-var VendingMachine = function() {
+var VendingMachine = function () {
     this.balance = 0;
-}
+};
 
-VendingMachine.prototype.getBalance = function() {
+VendingMachine.prototype.getBalance = function () {
     return this.balance;
-}
+};
 
-var assert = require('assert');
+let assert = require('assert');
 
-describe('Vending Machine', function() {
+describe('Vending Machine', function () {
 
-    describe('Balance of money inserted', function() {
-        it('Is zero when initially powered up', function() {
+    describe('Balance of money inserted', function () {
+        it('Is zero when initially powered up', function () {
             machine = new VendingMachine();
-            assert.equal(0, machine.getBalance())
+            assert.strictEqual(0, machine.getBalance())
         });
     });
 });
