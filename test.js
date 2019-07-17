b3253a81e528b3e04fc130b25addf37de4168675
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-17 15:45:37 +0100

    passes test to add a nickel

--- test.js
+++ test.js
@@ -2,11 +2,14 @@ var VendingMachine = function () {
     this.balance = 0;
 };
 
-// TEST
 
 VendingMachine.prototype.getBalance = function () {
     return this.balance;
 };
+VendingMachine.prototype.addNickel = function () {
+    return this.balance += 5;
+};
+
 
 let assert = require('assert');
 
@@ -16,6 +19,13 @@ describe('Vending Machine', function () {
         it('Is zero when initially powered up', function () {
             machine = new VendingMachine();
             assert.strictEqual(0, machine.getBalance())
+        });
+    });
+    describe('Balance of money inserted', function () {
+        it('Is 5 when nickel is added', function () {
+            machine = new VendingMachine();
+            machine.addNickel();
+            assert.strictEqual(5, machine.getBalance())
         });
     });
 });
